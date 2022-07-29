fc9d4187ce4c42332ac699271a0fd2f7ffbbcd85
Mark Gallant <markgallant01@gmail.com>
2022-07-29 07:00:20 -0400

    complete problem 24

--- problems/24-my-simple-reduce.js
+++ problems/24-my-simple-reduce.js
@@ -34,8 +34,13 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   optional initial accumulator
 *******************************************************************************/
 
-let mySimpleReduce = function() {
+let mySimpleReduce = function(arr, callback) {
+  let accumulator = arr[0];
+  for (let i=1; i<arr.length; i++) {
+    accumulator = callback(accumulator, arr[i]);
+  }
 
+  return accumulator;
 };
 
 
