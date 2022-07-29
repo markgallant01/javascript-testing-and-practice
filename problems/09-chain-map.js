857f7815da4c2c49308c68d9127a79b01077afc7
Mark Gallant <markgallant01@gmail.com>
2022-07-29 06:11:07 -0400

    complete problem 09

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -29,8 +29,12 @@ console.log(chainMap(4, square, half));         // 8
 console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
-let chainMap = function() {
+let chainMap = function(value, ...callbacks) {
+  callbacks.forEach(function(callback) {
+    value = callback(value);
+  });
 
+  return value;
 };
 
 
