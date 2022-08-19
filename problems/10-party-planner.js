2284bbac5b5afbb0b1dcd7d3510a62f2509cdfa7
Mark Gallant <markgallant01@gmail.com>
2022-08-19 19:19:41 -0400

    complete problem 10

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,28 @@
 // Your code here
+class PartyPlanner {
+	constructor() {
+		this.guestList = [];
+	}
 
+	addToGuestList(name) {
+		this.guestList.push(name);
+	}
+
+	throwParty() {
+		if (this.guestList.length === 0) {
+			return 'Gotta add people to the guest list';
+		}
+		else {
+			let returnString = "Welcome to the party " + this.guestList[0];
+			for (let i = 1; i < this.guestList.length; i++) {
+				returnString += " and " + this.guestList[i];
+			}
+
+			return returnString;
+		}
+	}
+
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
