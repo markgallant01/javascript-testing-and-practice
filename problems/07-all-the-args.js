4b011f39136fc273b47b45f668ce16cb8647b601
Mark Gallant <markgallant01@gmail.com>
2022-08-19 18:54:51 -0400

    complete problem 07

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,5 +1,8 @@
 function allTheArgs(func, ...args) {
   // Your code here
+  return (...moreArgs) => {
+    return func(...args, ...moreArgs);
+  }
 }
 
 /*****************************************************************************/
