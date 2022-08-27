9f32354846d6f98115799c49c9432251e1ace34d
Mark Gallant <markgallant01@gmail.com>
2022-08-27 17:42:32 -0400

    implement movement functions and pass all tests

--- class/cursor.js
+++ class/cursor.js
@@ -24,10 +24,43 @@ class Cursor {
 
   left() {
     // Move cursor left
+    if (this.col > 0) {
+      this.resetBackgroundColor();
+      this.col -= 1;
+      this.setBackgroundColor();
+      Screen.render();
+    }
   }
 
   right() {
     // Move cursor right
+    let limit = this.numCols - 1;
+    if (this.col < limit) {
+      this.resetBackgroundColor();
+      this.col += 1;
+      this.setBackgroundColor();
+      Screen.render();
+    }
+  }
+
+  up() {
+    // move cursor up
+    if (this.row > 0) {
+      this.resetBackgroundColor();
+      this.row -= 1;
+      this.setBackgroundColor();
+      Screen.render();
+    }
+  }
+
+  down() {
+    let limit = this.numRows - 1;
+    if (this.row < limit) {
+      this.resetBackgroundColor();
+      this.row += 1;
+      this.setBackgroundColor();
+      Screen.render();
+    }
   }
 
 }
