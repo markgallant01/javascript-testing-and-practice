56b5729de53ae021ae2db58a367d2fa587889259
Mark Gallant <markgallant01@gmail.com>
2022-08-26 20:02:48 -0400

    implement game logic to pass all tests

--- class/ttt.js
+++ class/ttt.js
@@ -35,6 +35,83 @@ class TTT {
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
 
+    let winner;
+    // check for horizontal wins
+    grid.forEach(row => {
+      if (row[0] === 'X' &&
+          row[1] === 'X' &&
+          row[2] === 'X') {
+        winner = 'X';
+      }
+      else if (row[0] === 'O' &&
+               row[1] === 'O' &&
+               row[2] === 'O') {
+        winner = 'O';
+      }
+    });
+
+    // check for vertical wins
+    for (let i = 0; i < grid.length; i++) {
+      if (grid[0][i] === 'X' &&
+          grid[1][i] === 'X' &&
+          grid[2][i] === 'X') {
+        winner = 'X';
+      }
+      else if (grid[0][i] === 'O' &&
+               grid[1][i] === 'O' &&
+               grid[2][i] === 'O') {
+        winner = 'O';
+      }
+    }
+
+    // check for diagonal wins
+    // only two possible diagonal win conditions
+    // first check for X
+    if (grid[0][0] === 'X' &&
+        grid[1][1] === 'X' &&
+        grid[2][2] === 'X') {
+      winner = 'X';
+    }
+    else if (grid[0][2] === 'X' &&
+             grid[1][1] === 'X' &&
+             grid[2][0] === 'X') {
+      winner = 'X';
+    }
+
+    // check same conditions for O
+    if (grid[0][0] === 'O' &&
+        grid[1][1] === 'O' &&
+        grid[2][2] === 'O') {
+      winner = 'O';
+    }
+    else if (grid[0][2] === 'O' &&
+             grid[1][1] === 'O' &&
+             grid[2][0] === 'O') {
+      winner = 'O';
+    }
+
+    // if there's no winner then check for a tie
+    if (!winner) {
+      let tie = true;
+      grid.forEach(row => {
+        row.forEach(block => {
+          if (block === ' ') {
+            tie = false;
+          }
+        });
+      });
+
+      if (tie) {
+        winner = 'T';
+      }
+    }
+
+    if (winner) {
+      return winner;
+    }
+    else {
+      return false;
+    }
   }
 
   static endGame(winner) {
