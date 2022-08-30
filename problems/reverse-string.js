a5c8df575d2b63036991dd284ab5115c695aaeca
Mark Gallant <markgallant01@gmail.com>
2022-08-29 21:40:16 -0400

    complete spec and implementation of reverseString()

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,12 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string === 'string') {
+    string = string.split('');
+    string = string.reverse();
+    
+    return string.join('');
+  }
+  else {
+    throw new TypeError('must be type: string');
+  }
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,15 @@
-// Your code here
+const { expect } = require('chai');
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString()', function() {
+
+  it('should return a reversed string', function() {
+    expect(reverseString('fun')).to.equal('nuf');
+  });
+
+  it('should throw a TypeError when not called on string', function() {
+    expect(() => reverseString(5)).to.throw();
+  });
+
+});
+
