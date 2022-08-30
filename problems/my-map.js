d5fa59cd4e744b8eddf3a0a62184aad3ca0b9b79
Mark Gallant <markgallant01@gmail.com>
2022-08-29 22:54:52 -0400

    complete spec and implementation of myMap()

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,15 @@
 function myMap(inputArray, callback) {
   // Your code here
+  if (callback === undefined) {
+    callback = x => x;
+  }
+
+  let newArr = [];
+  inputArray.forEach((x) => {
+    newArr.push(callback(x));
+  });
+
+  return newArr;
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,47 @@
 // Your code here
+const chai = require('chai');
+const spies = require('chai-spies');
+chai.use(spies);
+const expect = chai.expect;
+
+const myMap = require('../problems/my-map');
+
+describe('myMap(arr, cb)', function() {
+  let a1;
+
+  beforeEach(function() {
+    a1 = [1,2,3];
+  });
+
+  it('should call cb on every element of arr', function() {
+    let a2 = myMap(a1, x => x * 2);
+
+    expect(a2).to.deep.equal([2,4,6]);
+  });
+
+  it('should return the input if no cb is provided', function() {
+    let a2 = myMap(a1);
+    
+    expect(a2).to.deep.equal([1,2,3]);
+  });
+
+  it('should not mutate the passed-in array', function() {
+    let a2 = myMap(a1, x => x * 2);
+
+    expect(a1).to.deep.equal([1,2,3]);
+  });
+
+  it('should not call Array.map()', function() {
+    const spy = chai.spy.on(a1, 'map');
+    expect(spy).to.not.have.been.called;
+  });
+
+  it('should invoke cb once for each element of arr', function() {
+    let l = a1.length;
+    const spyCB = chai.spy(x => x * 2);
+    let a2 = myMap(a1, spyCB);
+
+    expect(spyCB).to.have.been.called.exactly(l);
+  });
+});
+
