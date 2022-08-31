e271ec276ca78d0a0bb38dc696d3d3278879be11
Mark Gallant <markgallant01@gmail.com>
2022-08-31 15:38:18 -0400

    implement _randomMove() and _getValidMoves()

--- class/computer-player.js
+++ class/computer-player.js
@@ -3,23 +3,43 @@ class ComputerPlayer {
 
   static getValidMoves(grid) {
     // Your code here
+    let moves = [];
+
+    // loop through grid
+    for (let i = 0; i < grid.length; i++) {
+      let row = grid[i];
+      for (let j = 0; j < row.length; j++) {
+        // if current square is empty, push it to moves array
+        if (grid[i][j] === ' ') {
+          moves.push({ row: i, col: j });
+        }
+      }
+    }
+
+    return moves;
   }
 
   static randomMove(grid) {
+    // get list of valid moves
+    let moves = ComputerPlayer.getValidMoves(grid);
 
-    // Your code here
+    // pick a random move from moves array
+    let min = 0;
+    let max = moves.length - 1;
+    let move = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return moves[move];
   }
 
   static getWinningMoves(grid, symbol) {
 
-    // Your code here
 
   }
 
   static getSmartMove(grid, symbol) {
-
-    // Your code here
-
+    // check for any winning moves
+    // then check for any blocking moves
+    // then pick a random move
   }
 
 }
