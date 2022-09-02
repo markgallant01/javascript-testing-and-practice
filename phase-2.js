0edad7b612e43bd4d43d8684b4df6b9ab473567b
Mark Gallant <markgallant01@gmail.com>
2022-09-02 18:52:24 -0400

    implement addNums10() to spec

--- phase-2.js
+++ phase-2.js
@@ -3,7 +3,12 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let result = [];
+  let limit = increment * 10;
+  for (let i = increment; i <= limit; i += increment) {
+    result.push(addNums(i));
+  }
+  return result;
 }
 
 // Runs `addManyNums` in 10 increasing increments
