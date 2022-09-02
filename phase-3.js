2e713424a025200b154b8513a7bf69738878ad45
Mark Gallant <markgallant01@gmail.com>
2022-09-02 19:35:16 -0400

    add timing benchmarks

--- phase-3.js
+++ phase-3.js
@@ -2,21 +2,35 @@ const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
   // Copy your `addNums10` code here
-  // Then, add timing code
+  function addNums10(increment) {
+    // Fill this in
+    let result = [];
+    let limit = increment * 10;
+    for (let i = increment; i <= limit; i += increment) {
+      result.push(addNums(i));
+      console.timeLog('t');
+    }
+    return result;
+  }
 
-  // Your code here
-
+  console.time('t');
+  addNums10(increment);
+  console.timeEnd('t');
 }
-
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
-// Then, add timing code
-
-  // Your code here
+  function addManyNums10(increment) {
+    // Fill this in
+    let result = [];
+    let limit = increment * 10;
+    for (let i = increment; i <= limit; i += increment) {
+      result.push(addManyNums(i));
+    }
+    return result;
+  }
 
 }
-
 
 n = 1000000
 console.log(`addNums(${n}): `);
