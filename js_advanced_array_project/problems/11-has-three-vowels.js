a96530dcad65e781058d80c5ce737b67592b272f
Mark Gallant <markgallant01@gmail.com>
2022-09-13 20:16:02 -0400

    prepare for merge

---
+++ js_advanced_array_project/problems/11-has-three-vowels.js
@@ -0,0 +1,42 @@
+/*
+Write a function `hasThreeVowels` that accepts a string as an argument.
+The function should return a boolean indicating whether or not the string
+contains at least three different vowels.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+*/
+
+let hasThreeVowels = function(string) {
+    // Your code here
+    let vowels = 'aeiou';
+    let uniqueVowels = '';
+    let letters = string.split('');
+
+    letters.forEach(function(letter) {
+        if (vowels.includes(letter) && !uniqueVowels.includes(letter)) {
+            uniqueVowels += letter;
+        }
+    });
+
+    return uniqueVowels.length  >= 3;
+};
+
+// Your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hasThreeVowels;
+} catch (e) {
+    module.exports = null;
+}
---
+++ js_advanced_array_project/problems/16-hipsterfy.js
@@ -0,0 +1,59 @@
+/*
+
+Write a function `hipsterfy(sentence)` that takes in a sentence string and
+returns the sentence where every word is missing it's last vowel.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+
+*/
+
+let removeLastVowel = function(word) {
+    // Your code here
+    let vowels = 'aeiou';
+    let lastVowelIndex;
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i];
+        if (vowels.includes(letter)) {
+            lastVowelIndex = i;
+        }
+    }
+
+    let result = '';
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i];
+        if (i !== lastVowelIndex) {
+            result += letter;
+        }
+    }
+
+    return result;
+};
+
+let hipsterfy = function(sentence) {
+    // Your code here
+    let words = sentence.split(' ');
+    words = words.map(function(word) {
+        return removeLastVowel(word);
+    });
+    return words.join(' ');
+};
+
+// alternative solution using Array.map
+// let hipsterfy = function(sentence) {
+//     return sentence.split(' ').map(removeLastVowel).join(' ');
+// };
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hipsterfy;
+} catch (e) {
+    module.exports = null;
+}
